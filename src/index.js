d6c6b614371abaa794586a7492d36ba6ebe03972
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-05-23 18:08:45 +0600

    react router added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Home from './pages/Home/Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <div>
-   <h1>ho</h1>
- </div>
+   <BrowserRouter>
+   <Routes>
+     <Route path="/" element={<Home/>}/>
+     <Route path="/home" element={<Home/>}/>
+   </Routes>
+   </BrowserRouter>
+  </div>
  
 );
 
